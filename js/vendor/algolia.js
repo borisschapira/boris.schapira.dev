893febece65df71872fd2b34f6cd11fc3d39ba10
Boris SCHAPIRA <borisschapira@gmail.com>
2015-10-10 16:14:55 +0200

    Evolution

--- js/vendor/algolia.js
+++ js/vendor/algolia.js
@@ -24,7 +24,21 @@ $(function(config) {
     onQueryChange();
 	}
 
-  $searchInput.on('keyup', onQueryChange);
+  var delay = (function() {
+    var timer = 0;
+    return function(callback, ms) {
+      clearTimeout(timer);
+      timer = setTimeout(callback, ms);
+    };
+  })();
+
+  $searchInput.on('keyup', function(){
+    delay(function() {
+			if ($searchInput.val().length > 0) {
+				onQueryChange();
+			}
+		}, 150);
+  });
 
   // Content to hide/show when searching
   var $initialContent = $('.js-algolia__initial-content');
