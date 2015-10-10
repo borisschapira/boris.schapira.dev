9934a2b38cd11f11f9fd36c965155b3402632e65
Boris SCHAPIRA <borisschapira@gmail.com>
2015-10-10 16:55:02 +0200

    Evolutions

--- js/elevator.js
+++ js/elevator.js
@@ -2,7 +2,6 @@ window.onload = function() {
   var elevator = new Elevator({
     element: document.querySelector('.elevator'),
     targetElement: document.querySelector('#top'),
-    duration: 5000,
     mainAudio: '/assets/music/elevator.mp3',
     endAudio: '/assets/music/ding.mp3'
   });
