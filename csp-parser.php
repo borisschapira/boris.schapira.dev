34cbeb76854c6902547d5a706ed53606e3e013c1
Boris SCHAPIRA <borisschapira@gmail.com>
2015-10-13 09:59:28 +0200

    Avancement

--- csp-parser.php
+++ csp-parser.php
@@ -5,10 +5,21 @@
 // Dareboost wants it? Not a problem.
 header('X-Content-Type-Options: "nosniff"');
 
-// Specify the email address to send reports to
-define('EMAIL', 'borisschapira@gmail.com');
-// Specify the desired email subject for violation reports
-define('SUBJECT', 'CSP violation');
+// Mysql connexion
+@$lk = mysqli_connect("mysql-borisschapira.alwaysdata.net", "48271_csp_report", "reporting", "borisschapira_csp_reports");
+if (!$lk) {
+	echo "No DB.";
+	exit;
+}
+if (!mysqli_set_charset($lk, "utf8")) {
+    printf("No utf8: %s\n", mysqli_error($lk));
+    exit;
+}
+
+// // Specify the email address to send reports to
+// define('EMAIL', 'borisschapira@gmail.com');
+// // Specify the desired email subject for violation reports
+// define('SUBJECT', 'CSP violation');
 
 // Get the raw POST data
 $data = file_get_contents('php://input');
@@ -47,13 +58,28 @@ if ($data = json_decode($data, true)) {
      ) {
 
           // Prettify the JSON-formatted data
-          $data = json_encode(
-                    $data,
-                    JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
-                  );
+          $data_flat = json_encode(
+                   $data,
+                   JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+                 );
+          
+          // // Simply mail the CSP violation report
+          // mail(EMAIL, SUBJECT, $mail_data, 'Content-Type: text/plain;charset=utf-8');
 
-          // Simply mail the CSP violation report
-          mail(EMAIL, SUBJECT, $data, 'Content-Type: text/plain;charset=utf-8');
+          // Writing in DB
+          $query_insert = " INSERT INTO csp_reports SET ";
+          $query_insert.= " document_uri       = '".mysqli_real_escape_string( $lk , $data['csp-report']['document-uri'] )."', ";
+          $query_insert.= " full_report        = '".mysqli_real_escape_string( $lk , $data_flat )."', ";
+          $query_insert.= " referrer           = '".mysqli_real_escape_string( $lk , $data['csp-report']['referrer'] )."', ";
+          $query_insert.= " violated_directive = '".mysqli_real_escape_string( $lk , $data['csp-report']['violated-directive'] )."', ";
+          $query_insert.= " original_policy    = '".mysqli_real_escape_string( $lk , $data['csp-report']['original-policy'] )."', ";
+          $query_insert.= " blocked_uri 	   = '".mysqli_real_escape_string( $lk , $data['csp-report']['blocked-uri'] )."', ";
+          $query_insert.= " source_file        = '".mysqli_real_escape_string( $lk , $data['csp-report']['source-file'] )."', ";
+          $query_insert.= " line_number        = '".mysqli_real_escape_string( $lk , $data['csp-report']['line-number'] )."', ";
+          $query_insert.= " column_number      = '".mysqli_real_escape_string( $lk , $data['csp-report']['column-number'] )."', ";
+          $query_insert.= " status_code        = '".mysqli_real_escape_string( $lk , $data['csp-report']['status-code'] )."' ";
+
+          $obj = mysqli_query($lk, $query_insert);
       }
 
 }
