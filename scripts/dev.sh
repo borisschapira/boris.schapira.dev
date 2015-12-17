3e7624fa6ac6f7e52e1514a6ee0f4c97c0ee6e14
Boris SCHAPIRA <borisschapira@gmail.com>
2015-12-17 21:44:31 +0100

    Dev accessible sur le lan

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,7 +7,7 @@ mkdir _posts/papa;
 cp -R _posts_prod/2015 _posts;
 cp -R _posts_prod/papa/2015 _posts/papa;
 
-bundle exec jekyll serve --incremental;
+bundle exec jekyll serve --host 0.0.0.0 --incremental;
 
 rm -rf _posts;
 mv _posts_prod _posts;
