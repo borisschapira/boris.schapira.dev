7b178340501f24ece1b3468c54eea6e33350ade7
Boris SCHAPIRA <borisschapira@gmail.com>
2015-12-23 22:54:02 +0100

    Update JS libs

--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -1,12 +1,19 @@
-//= require vendors/jquery.2.1.4.min
-//= require vendors/algoliasearch.3.6.0.min
-//= require vendors/algoliasearch.helper.2.1.0.min
 //= require vendors/moment.2.10.3.min
-//= require vendors/algolia
-//= require vendors/cookies-monster
-//= require vendors/elevator.min
 //= require vendors/moment_fr
+
+//= require vendors/jquery.2.1.4.min
+
 //= require vendors/featherlight.min
+
+//= require vendors/algoliasearch.3.10.2.jquery.min
+//= require vendors/algoliasearch.helper.2.8.0.min
+
+//= require vendors/algolia
+
+//= require vendors/cookies-monster
+
+//= require vendors/elevator.min
+
 //= require analytics
 
 /* Elevator.js */
