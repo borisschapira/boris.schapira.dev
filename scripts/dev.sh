5204781f5a519b26f4abc2d1126c5b2d6fac30df
Boris SCHAPIRA <borisschapira@gmail.com>
2015-12-25 19:31:02 +0100

    Brouillons et articles futurs dans le dev

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,7 +7,7 @@ mkdir _posts/papa;
 cp -R _posts_prod/2015 _posts;
 cp -R _posts_prod/papa/2015 _posts/papa;
 
-bundle exec jekyll serve --host 0.0.0.0 --incremental;
+bundle exec jekyll serve --host 0.0.0.0 --incremental --drafts --future;
 
 rm -rf _posts;
 mv _posts_prod _posts;
