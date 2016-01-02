a72e64cb23efbca89b56ca26b928c61d21df26d0
Boris SCHAPIRA <borisschapira@gmail.com>
2016-01-02 14:21:23 +0100

    Précédent/suivant de catégorie

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,9 +4,9 @@ mv _posts _posts_prod;
 mkdir _posts;
 mkdir _posts/papa;
 
-cp -R _posts_prod/2015 _posts;
+cp -R _posts_prod/2016 _posts;
 cp -R _posts_prod/_images _posts/_images;
-cp -R _posts_prod/papa/2015 _posts/papa;
+cp -R _posts_prod/papa/2016 _posts/papa;
 
 bundle exec jekyll serve --host 0.0.0.0 --incremental --drafts --future;
 
