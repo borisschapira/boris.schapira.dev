6ceeb844ad7a05fa314e5f3c75523811d2986981
Boris SCHAPIRA <borisschapira@gmail.com>
2016-01-04 13:16:52 +0100

    Amélioration de l'include post concernant les catégories

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,19 +1,3 @@
 export JEKYLL_ENV=developpement
 
-mv _posts _posts_prod
-mkdir _posts
-mkdir _posts/papa
-
-cd _posts
-ln -s ../_posts_prod/2016 2016
-ln -s ../_posts_prod/_images _images
-
-cd papa
-ln -s ../../_posts_prod/papa/2016 2016
-
-cd ../..
-
-bundle exec jekyll serve --host 0.0.0.0 --incremental --drafts --future
-
-rm -rf _posts
-mv _posts_prod _posts
+bundle exec jekyll serve --limit_posts 30 --host 0.0.0.0 --incremental --drafts --future
