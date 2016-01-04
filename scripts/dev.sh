4cede1b53dc251bbd91b0ee9699983111f91521a
Boris SCHAPIRA <borisschapira@gmail.com>
2016-01-04 12:04:22 +0100

    Evol script dev

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,14 +1,19 @@
-export JEKYLL_ENV=developpement;
+export JEKYLL_ENV=developpement
 
-mv _posts _posts_prod;
-mkdir _posts;
-mkdir _posts/papa;
+mv _posts _posts_prod
+mkdir _posts
+mkdir _posts/papa
 
-cp -R _posts_prod/2016 _posts;
-cp -R _posts_prod/_images _posts/_images;
-cp -R _posts_prod/papa/2016 _posts/papa;
+cd _posts
+ln -s ../_posts_prod/2016 2016
+ln -s ../_posts_prod/_images _images
 
-bundle exec jekyll serve --host 0.0.0.0 --incremental --drafts --future;
+cd papa
+ln -s ../../_posts_prod/papa/2016 2016
 
-rm -rf _posts;
-mv _posts_prod _posts;
+cd ../..
+
+bundle exec jekyll serve --host 0.0.0.0 --incremental --drafts --future
+
+rm -rf _posts
+mv _posts_prod _posts
