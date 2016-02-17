fb5e561c1885beec0fb6544a9fee1fccb31b8df3
Boris SCHAPIRA <borisschapira@gmail.com>
2016-02-17 21:44:12 +0100

    :bug: Fix typo in script

--- scripts/webmentions.sh
+++ scripts/webmentions.sh
@@ -2,4 +2,4 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 export JEKYLL_ENV=production;
 
-Bundle exec rake --rakefile ./webmention.Rakefile webmention
+bundle exec rake --rakefile ./webmention.Rakefile webmention
