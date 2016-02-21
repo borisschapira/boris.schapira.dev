e529d1bd5f9f278cc27d2be02e3c798f8f229ea5
Boris SCHAPIRA <borisschapira@gmail.com>
2016-02-21 12:01:11 +0100

    Abbr accessibility

--- _assets/scripts/instant.js
+++ _assets/scripts/instant.js
@@ -1,3 +1,12 @@
 //= require vendors/instantclick.min
 
+//= require vendors/touchtap-event.js
+//= require vendors/abbr-touch.js
+
 InstantClick.init(50);
+
+InstantClick.on('change', onChange);
+
+function onChange(isInitialChange){
+    abbrTouch();
+}
--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -1,15 +1,2 @@
 //= require vendors/cookies-monster
-
-//= require vendors/elevator.min
-
 //= require analytics
-
-/* Elevator.js */
-window.onload = function() {
-  var elevator = new Elevator({
-    element: document.querySelector('.elevator'),
-    targetElement: document.querySelector('#top'),
-    mainAudio: '/assets/music/elevator.mp3',
-    endAudio: '/assets/music/ding.mp3'
-  });
-}
