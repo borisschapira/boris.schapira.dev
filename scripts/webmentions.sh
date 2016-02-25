dd3f67f97512a0fd4e834df9db189a762ebfbbe5
Boris SCHAPIRA <borisschapira@gmail.com>
2016-02-25 11:17:45 +0100

    Move rake tasks to tasks folder

--- scripts/webmentions.sh
+++ scripts/webmentions.sh
@@ -2,4 +2,4 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 export JEKYLL_ENV=production;
 
-bundle exec rake --rakefile ./webmention.Rakefile webmention
+bundle exec rake webmention
