2bc4ce1de6d1a4873fed3908ceb9418ed3a95625
Boris SCHAPIRA <borisschapira@gmail.com>
2017-01-18 20:42:44 +0100

    Piwik

--- _assets/scripts/analytics.js
+++ _assets/scripts/analytics.js
@@ -17,3 +17,17 @@ if (document.getElementById('data-analytics')) {
     ga('send', 'pageview');
   }
 }
+
+/* Piwik */
+var _paq = _paq || [];
+// tracker methods like "setCustomDimension" should be called before "trackPageView"
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//filature.borisschapira.com/";
+  _paq.push(['setTrackerUrl', u+'p']);
+  _paq.push(['setSiteId', '1']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+/* End Piwik */
