06d358f52885cf4450c3c7c491c09984ce83158f
Boris SCHAPIRA <borisschapira@gmail.com>
2017-01-19 12:21:47 +0100

    Piwik through Ghostery

--- _assets/scripts/analytics.js
+++ _assets/scripts/analytics.js
@@ -28,6 +28,6 @@ _paq.push(['enableLinkTracking']);
   _paq.push(['setTrackerUrl', u+'p']);
   _paq.push(['setSiteId', '1']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'suivre'; s.parentNode.insertBefore(g,s);
 })();
 /* End Piwik */
