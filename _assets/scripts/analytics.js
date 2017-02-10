6bc385c8aa0e108220f0d06e17c33378495a4b8f
Boris SCHAPIRA <borisschapira@gmail.com>
2017-02-10 15:44:06 +0100

    Remove GA

--- _assets/scripts/analytics.js
+++ _assets/scripts/analytics.js
@@ -1,23 +1,3 @@
-
-/* Hacked isogram with http://isogrammer.com/ */
-(function (d,e,v,i,a,n,t) {
-  d['GoogleAnalyticsObject'] = a;d[a] = d[a] || function () {
-(d[a].q = d[a].q || []).push(arguments);},d[a].l = 1 * new Date();n = e.createElement(v),
-  t = e.getElementsByTagName(v)[0];n.async = 1;n.src = i;t.parentNode.insertBefore(n, t);
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-if (document.getElementById('data-analytics')) {
-  var gadata = JSON.parse(document.getElementById('data-analytics').value);
-
-  if (gadata.environment == 'production') {
-    ga('set', 'dimension1', gadata.categories);
-    ga('set', 'dimension2', gadata.tags);
-
-    ga('create', 'UA-4257717-9', 'auto');
-    ga('send', 'pageview');
-  }
-}
-
 /* Piwik */
 var _paq = _paq || [];
 // tracker methods like "setCustomDimension" should be called before "trackPageView"
