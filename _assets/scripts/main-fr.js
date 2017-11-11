511618afbf7bd1f47d69340f04868bf88d2db8f3
Boris SCHAPIRA <borisschapira@gmail.com>
2017-11-11 23:27:36 +0100

    Simplified script --skip-ci

--- _assets/scripts/main-fr.js
+++
@@ -1,9 +0,0 @@
-//= require vendors/moment.2.10.3.min
-//= require vendors/moment_fr
-//= require vendors/zepto.1.2.0.min.js
-
-// Replace datetimes by moments relative values
-moment.locale('fr');
-document.querySelectorAll('time[datetime]').forEach(function(item){
-    item.textContent = moment(item.getAttribute('datetime'), moment.ISO_8601).fromNow()
-})
