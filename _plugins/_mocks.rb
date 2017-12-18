62e8692465ddd8dc26eb57da1aa97e5c079fb54c
Boris SCHAPIRA <borisschapira@gmail.com>
2017-12-18 11:02:31 +0100

    Mock class for testing purposes

---
+++ _plugins/_mocks.rb
@@ -0,0 +1,43 @@
+# module Jekyll
+#   class AssetMock < Liquid::Tag
+#     def initialize(tag_name, text, tokens)
+#       super
+#       @text = text.strip
+#     end
+
+#     def render(context)
+#       @text
+#     end
+#   end
+# end
+
+# module Jekyll
+#   class CloudinaryMock < Liquid::Tag
+#     def initialize(tag_name, text, tokens)
+#       super
+#       @text = text.strip
+#     end
+
+#     def render(context)
+#       @text
+#     end
+#   end
+# end
+
+# module Jekyll
+#   class ResponsiveImageMock < Liquid::Tag
+#     def initialize(tag_name, text, tokens)
+#       super
+#       @text = text.strip
+#     end
+
+#     def render(context)
+#       @text
+#     end
+#   end
+# end
+  
+# Liquid::Template.register_tag('asset', Jekyll::AssetMock)
+# Liquid::Template.register_tag('cloudinary', Jekyll::CloudinaryMock)
+# Liquid::Template.register_tag('responsive_image', Jekyll::ResponsiveImageMock)
+  
