a819be8f7bc353d8c1d333dc42a79536912a1300
Boris SCHAPIRA <borisschapira@gmail.com>
2018-03-06 22:46:00 +0100

    Lang switcher

--- _src/scripts/custom/index.js
+++ _src/scripts/custom/index.js
@@ -1,3 +1,21 @@
+(function switchlang() {
+    // Detect user language and redirect, if first time, to the right page ----------------
+    try {
+        var lang_user;
+        lang_user = localStorage.getItem("lang_user");
+        if (!lang_user) {
+            var lang_user = (window.navigator.userLanguage || window.navigator.language).slice(0, 2);
+            localStorage.setItem("lang_user", lang_user);
+            var lang_site = document.getElementsByTagName('html')[0].lang;
+            if (lang_user != lang_site) {
+                window.location = document.querySelector('[rel="alternate"]').href;
+            }
+        }
+    } catch (e) {
+        console.log("No localstorage, no lang redirection.")
+    }
+})();
+
 function ready(fn) {
     if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
         fn();
