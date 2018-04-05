b21339c2e4d012870f3fd1fb0815311c37a8dad9
Boris SCHAPIRA <borisschapira@gmail.com>
2018-04-05 22:31:49 +0200

    inlining search CSS and concat scripts

---
+++ _src/scripts/custom/search.js
@@ -0,0 +1,40 @@
+var search = instantsearch({
+    appId: document.getElementById('algolia-app-id').innerHTML,
+    apiKey: document.getElementById('algolia-api-key').innerHTML,
+    indexName: document.getElementById('algolia-index-name').innerHTML,
+    urlSync: true
+});
+
+search.addWidgets(
+    [
+    instantsearch.widgets.refinementList({
+        container: '#refinement-list',
+        attributeName: 'categories'
+    }),
+    
+    instantsearch.widgets.searchBox({
+        container: '#search-box',
+        placeholder: 'Recherche',
+        autofocus: false,
+        poweredBy: true,
+        reset: true,
+        loadingIndicator: false
+    }),
+    
+    instantsearch.widgets.hits({
+        container: '#hits',
+        templates: {
+            empty: 'Aucun résultat&nbsp;!',
+            item: document.getElementById('hit-template').innerHTML
+        }
+    }),
+
+    instantsearch.widgets.pagination({
+        container: '#pagination-container',
+        maxPages: 20,
+        scrollTo: false,
+        showFirstLast: false
+    })
+    ]
+);
+search.start();
