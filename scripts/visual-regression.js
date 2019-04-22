78258aed145f24fad181cdf190a61bd1848d4657
Boris SCHAPIRA <borisschapira@gmail.com>
2019-04-22 20:27:07 +0200

    fixup! Add visual regression testing with puppeteer

--- scripts/visual-regression.js
+++ scripts/visual-regression.js
@@ -1,17 +1,30 @@
+// Iteration from https://meowni.ca/posts/2017-puppeteer-tests/#the-thing-that-does-the-diffing (thanks a lot, @notwaldorf!)
+
 const expect = require('chai').expect;
 const fs = require('fs');
 const pixelmatch = require('pixelmatch');
 const PNG = require("pngjs").PNG
 const puppeteer = require('puppeteer');
-const diffImagePatch = "~/Projects/perso/git-diff-image/diff-image";
 const testDir = "./_captures/test",
   prodDir = "./_captures/reference",
   diffDir = "./_captures/diff";
 const testUrl = "https://boris.schapira.local:10443";
-const routes = {
-  home: '',
-  web: 'web/',
-  post: '/2030/01/test-typo/'
+const tests = {
+  fr: {
+    locale: 'fr-FR,fr',
+    routes: {
+      home: '',
+      web: 'web/',
+      post: '/2030/01/test-typo/'
+    }
+  },
+  en: {
+    locale: 'en-US,en',
+    routes: {
+      home: '',
+      citizen: '/en/citizen/',
+    }
+  }
 };
 const contexts = {
   mobile: {
@@ -25,10 +38,11 @@ const contexts = {
 };
 
 describe('👀 screenshots are correct', function () {
-  let polyserve, browser, page;
+  let browser, page;
 
   // This is ran when the suite starts up.
   before(async function () {
+
     // Create the test directory if needed. This and the prodDir
     // variables are global somewhere.
     if (!fs.existsSync(testDir)) fs.mkdirSync(testDir);
@@ -38,40 +52,49 @@ describe('👀 screenshots are correct', function () {
     let i, j;
     for (i = 0; i < rootDirs.length; i++) {
       let rootDir = rootDirs[i];
-      if (!fs.existsSync(`${rootDir}`)) {
-        fs.mkdirSync(`${rootDir}`);
-      }
-      for (let c in contexts) {
-        if (!fs.existsSync(`${rootDir}/${c}`)) {
-          fs.mkdirSync(`${rootDir}/${c}`);
+      for (let t in tests) {
+        for (let c in contexts) {
+          if (!fs.existsSync(`${rootDir}`)) {
+            fs.mkdirSync(`${rootDir}`);
+          }
+          if (!fs.existsSync(`${rootDir}/${t}_${c}`)) {
+            fs.mkdirSync(`${rootDir}/${t}_${c}`);
+          }
         }
       }
     }
   });
 
-  // This is ran before every test. It's where you start a clean browser.
-  beforeEach(async function () {
-    browser = await puppeteer.launch({
-      args: ['--lang=fr-FR,fr']
-    });
-    page = await browser.newPage();
-  });
-
   // This is ran after every test; clean up after your browser.
   afterEach(() => browser.close());
 
-  for (let c in contexts) {
-    describe(`${c} screen`, function () {
+  for (let t in tests) {
+
+    describe(`${t} tests`, function () {
+
+      // This is ran before every test. It's where you start a clean browser.
       beforeEach(async function () {
-        return page.setViewport({
-          width: contexts[c].width,
-          height: contexts[c].height
+        browser = await puppeteer.launch({
+          args: [`--lang=${tests[t].locale}`]
         });
+        page = await browser.newPage();
       });
 
-      for (let r in routes) {
-        it(r, async function () {
-          return takeAndCompareScreenshot(page, r, routes[r], c);
+      for (let c in contexts) {
+        let routes = tests[t].routes;
+        describe(`${c} screen`, function () {
+          beforeEach(async function () {
+            return page.setViewport({
+              width: contexts[c].width,
+              height: contexts[c].height
+            });
+          });
+
+          for (let r in routes) {
+            it(r, async function () {
+              return takeAndCompareScreenshot(page, r, routes[r], `${t}_${c}`);
+            });
+          }
         });
       }
     });
@@ -96,13 +119,13 @@ async function takeAndCompareScreenshot(page, route, routeUrl, filePrefix) {
   return compareScreenshots(fileName);
 }
 
-function compareScreenshots(fileName) {
+async function compareScreenshots(fileName) {
   return new Promise((resolve, reject) => {
     const img1 = fs.createReadStream(`${testDir}/${fileName}.png`).pipe(new PNG()).on('parsed', doneReading);
     const img2 = fs.createReadStream(`${prodDir}/${fileName}.png`).pipe(new PNG()).on('parsed', doneReading);
     const diff = new PNG({
       width: img2.width,
-      height: img2.height
+      height: Math.max(img1.height, img2.height)
     });
 
     let filesRead = 0;
@@ -111,14 +134,10 @@ function compareScreenshots(fileName) {
       // Wait until both files are read.
       if (++filesRead < 2) return;
 
-      // The files should be the same size.
-      expect(img1.width, 'image widths are the same').equal(img2.width);
-      expect(img1.height, 'image heights are the same').equal(img2.height);
-
       // Do the visual diff.
       const diff = new PNG({
-        width: img1.width,
-        height: img2.height
+        width: img2.width,
+        height: Math.max(img1.height, img2.height)
       });
       const numDiffPixels = pixelmatch(
         img1.data, img2.data, diff.data, img1.width, img1.height, {
@@ -128,6 +147,10 @@ function compareScreenshots(fileName) {
       if (numDiffPixels > 200) {
         diff.pack().pipe(fs.createWriteStream(`${diffDir}/${fileName}.png`));
       }
+
+      // The files should be the same size.
+      expect(img1.width, 'image widths are the same').equal(img2.width);
+      expect(img1.height, 'image heights are the same').equal(img2.height);
 
       // The files should look the same.
       expect(numDiffPixels, 'number of different pixels').below(200);
