e9ddc54861b61b9d4ad3fc427a60c59e549999bd
Boris SCHAPIRA <borisschapira@gmail.com>
2019-04-26 09:09:19 +0200

    Update eslint config

--- scripts/visual-regression.js
+++ scripts/visual-regression.js
@@ -1,14 +1,15 @@
 // Iteration from https://meowni.ca/posts/2017-puppeteer-tests/#the-thing-that-does-the-diffing (thanks a lot, @notwaldorf!)
 
-const expect = require('chai').expect;
 const fs = require('fs');
+const {expect} = require('chai');
 const pixelmatch = require('pixelmatch');
-const PNG = require("pngjs").PNG
+const {PNG} = require('pngjs');
 const puppeteer = require('puppeteer');
-const testDir = "./_captures/test",
-  prodDir = "./_captures/reference",
-  diffDir = "./_captures/diff";
-const testUrl = "https://boris.schapira.local:10443";
+
+const testDir = './_captures/test';
+const prodDir = './_captures/reference';
+const diffDir = './_captures/diff';
+const testUrl = 'https://boris.schapira.local:10443';
 const tests = {
   fr: {
     locale: 'fr-FR,fr',
@@ -22,7 +23,7 @@ const tests = {
     locale: 'en-US,en',
     routes: {
       home: '',
-      citizen: '/en/citizen/',
+      citizen: '/en/citizen/'
     }
   }
 };
@@ -37,26 +38,29 @@ const contexts = {
   }
 };
 
-describe('👀 screenshots are correct', function () {
-  let browser, page;
+describe('👀 screenshots are correct', () => {
+  let browser;
+  let page;
 
   // This is ran when the suite starts up.
-  before(async function () {
-
+  before(() => {
     // Create the test directory if needed. This and the prodDir
     // variables are global somewhere.
-    if (!fs.existsSync(testDir)) fs.mkdirSync(testDir);
+    if (!fs.existsSync(testDir)) {
+      fs.mkdirSync(testDir);
+    }
 
     // And its wide screen/small screen subdirectories.
     const rootDirs = [testDir, prodDir, diffDir];
-    let i, j;
-    for (i = 0; i < rootDirs.length; i++) {
-      let rootDir = rootDirs[i];
-      for (let t in tests) {
-        for (let c in contexts) {
-          if (!fs.existsSync(`${rootDir}`)) {
-            fs.mkdirSync(`${rootDir}`);
-          }
+    for (let i = 0; i < rootDirs.length; i++) {
+      const rootDir = rootDirs[i];
+      if (!fs.existsSync(`${rootDir}`)) {
+        fs.mkdirSync(`${rootDir}`);
+      }
+      
+      for (const t in tests) {
+        for (const c in contexts) {
+
           if (!fs.existsSync(`${rootDir}/${t}_${c}`)) {
             fs.mkdirSync(`${rootDir}/${t}_${c}`);
           }
@@ -68,30 +72,29 @@ describe('👀 screenshots are correct', function () {
   // This is ran after every test; clean up after your browser.
   afterEach(() => browser.close());
 
-  for (let t in tests) {
-
-    describe(`${t} tests`, function () {
-
+  for (const t in tests) {
+    describe(`${t} tests`, () => {
       // This is ran before every test. It's where you start a clean browser.
-      beforeEach(async function () {
+      beforeEach(async () => {
         browser = await puppeteer.launch({
+          headless: true,
           args: [`--lang=${tests[t].locale}`]
         });
         page = await browser.newPage();
       });
 
-      for (let c in contexts) {
-        let routes = tests[t].routes;
-        describe(`${c} screen`, function () {
-          beforeEach(async function () {
+      for (const c in contexts) {
+        const {routes} = tests[t];
+        describe(`${c} screen`, () => {
+          beforeEach(async () => {
             return page.setViewport({
               width: contexts[c].width,
               height: contexts[c].height
             });
           });
 
-          for (let r in routes) {
-            it(r, async function () {
+          for (const r in routes) {
+            it(r, async () => {
               return takeAndCompareScreenshot(page, r, routes[r], `${t}_${c}`);
             });
           }
@@ -106,7 +109,7 @@ describe('👀 screenshots are correct', function () {
 // - filePrefix is either "wide" or "narrow", since I'm automatically testing both.
 async function takeAndCompareScreenshot(page, route, routeUrl, filePrefix) {
   // If you didn't specify a file, use the name of the route.
-  let fileName = filePrefix + '/' + (route ? route : 'index');
+  const fileName = filePrefix + '/' + (route ? route : 'index');
 
   // Start the browser, go to the test page, and take a screenshot.
   await page.goto(`${testUrl}/${routeUrl}`);
@@ -120,19 +123,17 @@ async function takeAndCompareScreenshot(page, route, routeUrl, filePrefix) {
 }
 
 async function compareScreenshots(fileName) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     const img1 = fs.createReadStream(`${testDir}/${fileName}.png`).pipe(new PNG()).on('parsed', doneReading);
     const img2 = fs.createReadStream(`${prodDir}/${fileName}.png`).pipe(new PNG()).on('parsed', doneReading);
-    const diff = new PNG({
-      width: img2.width,
-      height: Math.max(img1.height, img2.height)
-    });
 
     let filesRead = 0;
 
     function doneReading() {
       // Wait until both files are read.
-      if (++filesRead < 2) return;
+      if (++filesRead < 2) {
+        return;
+      }
 
       // Do the visual diff.
       const diff = new PNG({
