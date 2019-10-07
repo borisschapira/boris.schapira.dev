b32bce8cbe71e410f9f496f21107a3fa7f88902e
Boris SCHAPIRA <borisschapira@gmail.com>
2019-10-07 10:16:16 +0200

    PostCSS config dev != prod

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ const autoprefixer = require("autoprefixer");
 const csswring = require("csswring");
 const purgeCss = require("postcss-purgecss");
 
-module.exports = {
-  plugins: [autoprefixer, purgeCss, csswring]
-};
+console.log(process.env.NODE_ENV);
+
+module.exports = ctx => ({
+  plugins: ctx.env === 'development' ?  [autoprefixer, csswring] : [autoprefixer, purgeCss, csswring]
+})
