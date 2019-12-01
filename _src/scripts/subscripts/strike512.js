cd95369c1ed70373fe37002e8d0bcb3dc7a6eaed
Boris SCHAPIRA <borisschapira@gmail.com>
2019-12-01 18:50:06 +0100

    Hide banner until JS is executed.

--- _src/scripts/subscripts/strike512.js
+++ _src/scripts/subscripts/strike512.js
@@ -4,6 +4,11 @@ try {
   if (lastTimestamp) {
     inputDisplay.checked = (lastTimestamp > new Date().setHours(0, 0, 0, 0));
   }
+
+  var node = document.createElement('style');
+  node.innerHTML = '.strike512-container{display:flex;}';
+  document.body.appendChild(node);
+
   inputDisplay.addEventListener('change', function() {
     localStorage.setItem('strike512-hide', Date.now());
   });
