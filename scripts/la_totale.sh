4f281bde9b01ab211c4171615c4e42d2025080c7
Boris SCHAPIRA <boris@schapira.dev>
2021-02-20 20:50:42 +0100

    Add browserslist db update

--- scripts/la_totale.sh
+++ scripts/la_totale.sh
@@ -1,1 +1,1 @@
-bundle up; npm up; bundle exec rake "build:generate[prod]"; npm run build; say "Site généré, à tester."
+bundle up; npm up; npx browserslist@latest --update-db; bundle exec rake "build:generate[prod]"; npm run build; say "Site généré, à tester."
