b746cefc3627069172bbdb719b4bd7d40171bdf7
Boris Schapira <boris@schapira.dev>
2024-10-30 19:03:06 +0100

    HTTP server at the end

--- scripts/la_totale.sh
+++ scripts/la_totale.sh
@@ -1,1 +1,2 @@
-bundle up; pnpm up; cd tests; bundle up; pnpm up; cd ..; npx browserslist@latest; bundle exec rake "build:generate[prod]"; pnpm run build; bundle exec rake "build:generate[prod]"; say "Site généré, à tester."
+bundle up; pnpm up; cd tests; bundle up; pnpm up; cd ..; npx browserslist@latest; bundle exec rake "build:generate[prod]"; pnpm run build; bundle exec rake "build:generate[prod]"; say "Site généré, à tester.";
+pnpn dlx http-server _site;
