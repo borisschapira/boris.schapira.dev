e3acb03c8dffec93dfc4abeb4c7a22a8233ef623
Boris Schapira <boris@schapira.dev>
2025-01-28 13:47:16 +0100

    Test: remove Service Worker

--- _src/sw/subscripts/no-cache-matomo.js
+++
@@ -1,5 +0,0 @@
-import { registerRoute } from 'workbox-routing';
-import { NetworkOnly } from 'workbox-strategies';
-
-// Never cache Matomo calls
-registerRoute(/^https:\/\/analytics\.schapira\.dev/, new NetworkOnly());
